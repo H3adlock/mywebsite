f2237ce0f702126cba17ab43534590b98f4edace
H3adlock <sourav.ghosh700@hotmail.com>
2019-01-21 13:04:25 +0530

    update views

--- alarmdesk/views.py
+++ alarmdesk/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("<h1>This is alarmdesk homepage</h1>")
